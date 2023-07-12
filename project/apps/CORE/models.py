61d8602fbb8ffe3f5171a4be8ed023fab7966ce6
Moreira Lucas <lucaasmoreira1996@gmail.com>
2023-07-12 20:31:32 -0300

    Add:Modelo Estudiante,Profesor,Curso

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+       nombre = models.CharField(max_length=100)
+       edad = models.IntegerField()
+       # Otros campos de tu modelo Estudiante
+
+class Profesor(models.Model):
+       nombre = models.CharField(max_length=100)
+       especialidad = models.CharField(max_length=100)
+       # Otros campos de tu modelo Profesor
+
+class Curso(models.Model):
+       nombre = models.CharField(max_length=100)
+       descripcion = models.TextField()
+       # Otros campos de tu modelo Curso
