7c98c42bfb69bc9304767da11bb038d32b4ec969
Moreira Lucas <lucaasmoreira1996@gmail.com>
2023-07-16 19:35:59 -0300

    otros cambios

--- project/apps/CORE/views.py
+++ project/apps/CORE/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Estudiante
+from .forms import EstudianteForm
+from django.http import HttpRequest,HttpResponse
+
 # Create your views here.
 def home(request):
-    estudiantes_registros=Estudiante.objects.all()
+    estudiantes_registros = Estudiante.objects.all()
     contexto = {"estudiantes":estudiantes_registros}
     return render(request,"CORE/index.html",contexto)
+
+def crear_estudiante(request: HttpRequest ) -> HttpResponse:
+    
+    if request.method == "POST":
+        form = EstudianteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("CORE:home")
+    else:
+        form = EstudianteForm()
+    return render(request,"CORE/crear.html",{"form":form})
