2c50e5cec9877d14de2fb50349c4db760ab2749b
Moreira Lucas <lucaasmoreira1996@gmail.com>
2023-07-16 17:11:58 -0300

    Se agrego un form en CORE y su views

---
+++ project/apps/CORE/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Estudiante
+
+
+class EstudianteForm(forms.ModelForm):
+    class Meta:
+        model = Estudiante
+        fields = [ "nombre" , "edad"]
+
--- project/apps/CORE/urls.py
+++ project/apps/CORE/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home , crear_estudiante
 
 app_name="CORE"
 
 urlpatterns = [
     path("", home,name='home'),
+    path("crear/", crear_estudiante , name= "crear"),
 ]
